28963b66220abe09bd4ae492361466830c3851b1
dgd03146 <ibory1220@naver.com>
2024-03-10 15:43:58 +0000

    chore: set up vanilla extract to styles workspace #7

---
+++ packages/styles/globals.css.ts
@@ -0,0 +1,23 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('body', {
+  fontFamily: `'Clear Sans', 'Helvetica Neue', Arial, sans-serif`,
+  padding: 0,
+  margin: 0,
+  boxSizing: 'border-box',
+});
+
+globalStyle('a', {
+  textDecoration: 'none',
+  color: 'black',
+});
+
+globalStyle('list', {
+  padding: '0',
+  listStyle: 'none',
+});
+
+globalStyle('input', {
+  border: 'none',
+  outline: 'none',
+});
