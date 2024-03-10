acbd8be6f7513f26e52aa3ab287865b2aaa348e3
dgd03146 <ibory1220@naver.com>
2024-03-10 16:12:15 +0000

    chore: Add Vanilla-Extract next plugin to web workspace #7

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@repo/ui"],
-};
+const nextConfig = { transpilePackages: ['@repo/ui'] };
+
+module.exports = withVanillaExtract(nextConfig);
