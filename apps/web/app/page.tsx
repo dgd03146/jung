9577e17611eeffca4dcf5f758c5b74c964416a42
dgd03146 <ibory1220@naver.com>
2024-03-13 20:22:14 +0000

    chore: set up playwright #9

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,39 +1,12 @@
 import Button from '@imgeozi/design-system/components/Button/Button';
-import styles from './page.module.css';
+import Link from 'next/link';
 
-const LINKS = [
-  {
-    title: 'Docs',
-    href: 'https://turbo.build/repo/docs',
-    description: 'Find in-depth information about Turborepo features and API.',
-  },
-  {
-    title: 'Learn',
-    href: 'https://turbo.build/repo/docs/handbook',
-    description: 'Learn more about monorepos with our handbook.',
-  },
-  {
-    title: 'Templates',
-    href: 'https://turbo.build/repo/docs/getting-started/from-example',
-    description: 'Choose from over 15 examples and deploy with a single click.',
-  },
-  {
-    title: 'Deploy',
-    href: 'https://vercel.com/new',
-    description:
-      'Instantly deploy your Turborepo to a shareable URL with Vercel.',
-  },
-];
-
-export default function Page(): JSX.Element {
+export default function Page() {
   return (
-    <main className={styles.main}>
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
       <Button />
-      <div className={styles.grid}>
-        {LINKS.map(({ description }) => (
-          <p>{description}</p>
-        ))}
-      </div>
-    </main>
+    </div>
   );
 }
