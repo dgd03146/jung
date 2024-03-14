a293502252dd04cb7a774a75cc449722aa6071bd
dgd03146 <ibory1220@naver.com>
2024-03-14 02:55:19 +0000

    chore: set up biome & delete eslint #10

--- packages/design-system/tsup.config.ts
+++ packages/design-system/tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  treeshake: true,
-  entry: ['./**/*.tsx', './**/*.css.ts'],
-  format: ['esm'],
-  splitting: true,
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  minify: true,
-  external: ['react'],
+	treeshake: true,
+	entry: ['./**/*.tsx', './**/*.css.ts'],
+	format: ['esm'],
+	splitting: true,
+	dts: true,
+	sourcemap: true,
+	clean: true,
+	minify: true,
+	external: ['react'],
 });
