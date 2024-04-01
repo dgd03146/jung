cd713d1e3fb84124785fadcbafed8fadfbec5859
dgd03146 <ibory1220@naver.com>
2024-04-01 02:20:00 +0100

    design: Configure vanilla-extract design token #22

---
+++ packages/design-system/tokens/breakPoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+	sm: "360px",
+	md: "768px",
+	lg: "1080px",
+	xl: "1200px",
+	"2xl": "1920px",
+};
---
+++ packages/design-system/tokens/index.ts
@@ -0,0 +1,7 @@
+export { breakpoints } from "./breakPoints";
+export { fontFamily, fontSizes, fontWeights } from "./typography";
+export { opacity } from "./opacity";
+export { space, extendedSpace } from "./space";
+export { palette } from "./palette";
+export { borderRadius, borderStyles, borderWidths } from "./border";
+export { shadows } from "./shadows";
