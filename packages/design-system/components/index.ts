08e33f595f24cec832bbd73bd289224ca13fc90d
dgd03146 <ibory1220@naver.com>
2024-04-11 11:42:08 +0100

    rename: add grid Item to index.ts

--- packages/design-system/components/index.ts
+++ packages/design-system/components/index.ts
@@ -1,24 +1,17 @@
+export { GridItem } from './Grid/GridItem';
 export { Grid } from './Grid/Grid';
-
 export { Textarea } from './Textarea/Textarea';
-
 export { Input } from './Input/Input';
-
 export { Badge } from './Badge/Badge';
-
 export { Select } from './Select/Select';
-
 export { ListItem } from './List/ListItem';
 export { List } from './List/List';
-// --
-export type { ListItemComponent } from './List/ListItem';
-export type { ListComponent } from './List/List';
-
-export type { BoxProps } from './Box/Box';
 export { Box } from './Box/Box';
-
 export { button } from './Button/Button.css';
-
 export { Button } from './Button/Button';
 export { Flex } from './Flex/Flex';
 export { Stack } from './Stack/Stack';
+
+export type { ListItemComponent } from './List/ListItem';
+export type { ListComponent } from './List/List';
+export type { BoxProps } from './Box/Box';
