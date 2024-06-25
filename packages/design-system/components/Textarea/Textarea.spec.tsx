5783b117fe5303c1aeb3d0030ea50e7c09f66e5a
dgd03146 <ibory1220@naver.com>
2024-06-25 02:43:56 +0100

    test: Add unit test for Textarea component #50

---
+++ packages/design-system/components/Textarea/Textarea.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
+import userEvent from '@testing-library/user-event';
+import { Textarea } from './Textarea';
+
+describe('Textarea Component', () => {
+	const renderComponent = (props = {}) => {
+		return render(<Textarea {...props} />);
+	};
+
+	it('should call onChange handler when Textarea changes', async () => {
+		const handleChange = vi.fn();
+		renderComponent({ onChange: handleChange });
+		const textarea = screen.getByRole('textbox');
+
+		await userEvent.type(textarea, 'Hello');
+		expect(handleChange).toHaveBeenCalledTimes(5);
+		expect(textarea).toHaveValue('Hello');
+	});
+
+	it('should be disabled when disabled prop is true', () => {
+		renderComponent({ disabled: true });
+		const textarea = screen.getByRole('textbox');
+		expect(textarea).toBeDisabled();
+	});
+});
