bbe0e443ad4187d076c960e10ed4b949f447d100
dgd03146 <ibory1220@naver.com>
2024-07-08 05:47:13 +0100

    chore: Add Vanilla Extract plugin to tsup

--- packages/design-system/tsup.config.ts
+++ packages/design-system/tsup.config.ts
@@ -1,13 +1,15 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	treeshake: true,
-	entry: ['./**/*.tsx', './**/*.css.ts'],
-	format: ['esm'],
+	entry: ['./**/*.tsx'],
+	format: ['esm', 'cjs'],
 	splitting: true,
 	dts: true,
 	sourcemap: true,
 	clean: true,
 	minify: true,
 	external: ['react', 'react-dom'],
+	esbuildPlugins: [vanillaExtractPlugin()],
 });
