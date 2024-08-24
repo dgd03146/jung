b1b3b3346b57f95aad1db2cafad8cb6fe052517f
dgd03146 <ibory1220@naver.com>
2024-08-24 11:32:10 +0100

    chore: Configure Vite for path aliases and add font linktag #73

--- apps/admin/vite.config.mjs
+++ apps/admin/vite.config.mjs
@@ -3,12 +3,21 @@ import { defineConfig } from 'vite'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import baseConfig from "@jung/configs/vitest.config.mjs"
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
   test: {
     ...baseConfig.test,
     // 추가적인 Vitest 설정
+  },
+  resolve: {
+    alias: {
+      '@/fsd': path.resolve(__dirname, './src'),
+      '@jung/server': path.resolve(__dirname, '../../packages/server'),
+      '@jung/design-system': path.resolve(__dirname, '../../packages/design-system'),
+      '@jung/design-system/icons': path.resolve(__dirname, '../../packages/design-system/icons'),
+    },
   },
   // 기타 Vite 설정
 })
