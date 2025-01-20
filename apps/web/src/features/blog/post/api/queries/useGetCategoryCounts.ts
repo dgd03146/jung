e937ce22afeadd85fd37b3967bf6555f07fc24cd
dgd03146 <ibory1220@naver.com>
2025-01-20 09:07:47 +0900

    remove: Remove unnecessary useGetCategories hook

--- apps/web/src/features/blog/post/api/queries/useGetCategoryCounts.ts
+++
@@ -1,7 +0,0 @@
-import { trpc } from '@/fsd/shared';
-export function useGetCategoryCounts() {
-	return trpc.post.getCategoryCounts.useQuery(undefined, {
-		staleTime: 1000 * 60 * 60 * 6, // 6시간
-		gcTime: 1000 * 60 * 60 * 12, // 12시간
-	});
-}
