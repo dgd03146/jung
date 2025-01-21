82f2b79b04a296ff9dacbfc1ebe926c12ec738a7
dgd03146 <ibory1220@naver.com>
2025-01-21 16:26:12 +0900

    design: Implement not-found page

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -4,7 +4,50 @@
  * Unfortunately, the useDictionary hook cannot be used on this page
  * because the app/layout.tsx and app/not-found.tsx cannot be placed in app/(routing)/[lang]/ folder
  */
-const NotFound = () => {
-	return <div>404 - Not Found Page</div>;
-};
-export default NotFound;
+import { Box, Button, Container, Flex, Typography } from '@jung/design-system';
+import Link from 'next/link';
+import { MdOutlineSearchOff } from 'react-icons/md';
+
+export default function NotFound() {
+	return (
+		<Container>
+			<Flex
+				direction='column'
+				align='center'
+				justify='center'
+				gap='8'
+				paddingY='32'
+				minHeight='screenDvh'
+			>
+				<Box
+					padding='4'
+					borderRadius='half'
+					background='primary50'
+					color='primary'
+				>
+					<MdOutlineSearchOff size={48} />
+				</Box>
+
+				<Flex direction='column' align='center' gap='6' textAlign='center'>
+					<Typography.Heading level={1} color='primary'>
+						404
+					</Typography.Heading>
+
+					<Typography.Heading level={2} color='black100'>
+						Can't find the page
+					</Typography.Heading>
+
+					<Typography.Text level={1} color='black100'>
+						The page you requested has been deleted or the path is incorrect
+					</Typography.Text>
+				</Flex>
+
+				<Link href='/'>
+					<Button variant='primary' size='lg' borderRadius='lg'>
+						<Typography.Text fontWeight='medium'>Go to home</Typography.Text>
+					</Button>
+				</Link>
+			</Flex>
+		</Container>
+	);
+}
