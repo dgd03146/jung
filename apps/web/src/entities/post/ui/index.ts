14e15494ebdad3b71bd26cb489c138f80fe84539
dgd03146 <ibory1220@naver.com>
2025-01-29 00:48:19 +0900

    remove: Remove unused files

--- apps/web/src/entities/post/ui/index.ts
+++ apps/web/src/entities/post/ui/index.ts
@@ -1,4 +1,2 @@
-export { default as PostContent } from './PostContent';
 export { default as PostHeader } from './PostHeader';
-export { default as PostSidebar } from './PostSidebar';
 export { default as PostListSkeleton } from './PostListSkeleton';
