34691d3045e1e9b8eb355519effd062ccbd5e40e
dgd03146 <ibory1220@naver.com>
2025-01-31 14:09:36 +0900

    refactor: separate useinfinitescroll hook for reusable

--- apps/web/src/shared/model/index.ts
+++ apps/web/src/shared/model/index.ts
@@ -3,3 +3,5 @@ export { useSupabaseAuth } from './useSupabaseAuth';
 export { useInViewAnimation } from './useInviewAnimation';
 export { DictionaryContext } from './dictionaryContext';
 export type { RouteItem, RouteMap, RouteList } from './routes';
+export { useSearchParamsState } from './useSearchParamsState';
+export { useInfiniteScroll } from './useInfiniteScroll';
