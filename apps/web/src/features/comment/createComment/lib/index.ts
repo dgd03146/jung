ed9fe8754af0c53eca1a6c3ea2131bb31c86748e
dgd03146 <ibory1220@naver.com>
2025-02-04 14:05:16 +0900

    chore: add barrel exports for create comment feature utilities

---
+++ apps/web/src/features/comment/createComment/lib/index.ts
@@ -0,0 +1,4 @@
+export { createOptimisticComment } from './createOptimisticComment';
+export { rollbackOptimisticUpdate } from './rollbackOptimisticUpdate';
+export { updateOptimisticComment } from './updateOptimisticComment';
+export { replaceOptimisticWithReal } from './replaceOptimisticWithReal';
