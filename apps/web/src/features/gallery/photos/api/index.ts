56e5339d2a14821b32743d547e4e3954e6e84bb1
dgd03146 <ibory1220@naver.com>
2025-02-07 11:07:45 +0900

    refactor: cleanup photo features for FSD reorganization

--- apps/web/src/features/gallery/photos/api/index.ts
+++
@@ -1,6 +0,0 @@
-export { useGetPhotos } from './useGetPhotos';
-export { useGetPhotoById } from './useGetPhotoById';
-export { useGetAdjacentPhotos } from './useGetAdjacentPhotos';
-export { toggleLikePhotoAction } from './actions/toggleLikePhotoAction';
-export { useGetCollections } from './useGetCollections';
-export { useGetCollectionById } from './useGetCollectionById';
--- apps/web/src/features/gallery/photos/api/useGetAdjacentPhotos.ts
+++
@@ -1,8 +0,0 @@
-import { trpc } from '@/fsd/shared';
-
-export function useGetAdjacentPhotos(id: string) {
-	return trpc.photos.getAdjacentPhotos.useSuspenseQuery(id, {
-		staleTime: 1000 * 60 * 5,
-		gcTime: 1000 * 60 * 30,
-	});
-}
