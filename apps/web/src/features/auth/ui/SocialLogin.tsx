12fb1ada8b7945b5bdaf4ec55b0cc33309073aa4
dgd03146 <ibory1220@naver.com>
2025-02-12 20:46:45 +0900

    feat: add github login

--- apps/web/src/features/auth/ui/SocialLogin.tsx
+++ apps/web/src/features/auth/ui/SocialLogin.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { Button, Flex } from '@jung/design-system';
+import { Button, Flex, useToast } from '@jung/design-system';
+import { usePathname } from 'next/navigation';
 import { FcGoogle } from 'react-icons/fc';
 import { SiKakaotalk } from 'react-icons/si';
+import { VscGithub } from 'react-icons/vsc';
 import { useAuth } from '../model';
+import type { SocialProvider } from '../model/SocialProvider';
 import * as styles from './SocialLogin.css';
 
 interface SocialLoginProps {
@@ -12,16 +15,18 @@ interface SocialLoginProps {
 
 export function SocialLogin({ onSuccess }: SocialLoginProps) {
 	const { handleSocialLogin } = useAuth();
+	const pathname = usePathname();
+	const showToast = useToast();
 
-	const handleLogin = async (provider: 'google' | 'kakao') => {
+	const handleLogin = async (provider: SocialProvider) => {
 		try {
-			await handleSocialLogin(provider);
+			await handleSocialLogin(provider, { redirectTo: pathname });
 
 			if (onSuccess) {
 				onSuccess();
 			}
 		} catch (error) {
-			console.error('Login error:', error);
+			showToast('failed to login', 'error');
 		}
 	};
 
@@ -41,6 +46,13 @@ export function SocialLogin({ onSuccess }: SocialLoginProps) {
 			>
 				<SiKakaotalk className={styles.iconStyle} color='#000000' />
 				Kakao
+			</Button>
+			<Button
+				className={styles.loginOption.github}
+				onClick={() => handleLogin('github')}
+			>
+				<VscGithub className={styles.iconStyle} color='#FFFFFF' />
+				GitHub
 			</Button>
 		</Flex>
 	);
