d1fda0f7230666dec7ffeff03bf614f46d044d04
dgd03146 <ibory1220@naver.com>
2025-02-13 20:41:45 +0900

    fix: modify categry as category_id

--- apps/admin/src/features/blog/lib/validatePostData.ts
+++ apps/admin/src/features/blog/lib/validatePostData.ts
@@ -6,7 +6,7 @@ export const validatePostData = (postData: PostWithBlockContent): Errors => {
 		imagesrc: !postData.imagesrc ? 'Featured image is required' : '',
 		title: !postData.title.trim() ? 'Title is required' : '',
 		description: !postData.description.trim() ? 'Description is required' : '',
-		category: !postData.category ? 'Category is required' : '',
+		category_id: !postData.category_id.trim() ? 'Category is required' : '',
 		date: !postData.date ? 'Date is required' : '',
 	};
 };
