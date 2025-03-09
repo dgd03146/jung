a6b47d30c205734334307247bad9847ab2e7c744
dgd03146 <ibory1220@naver.com>
2025-03-10 07:50:37 +0900

    feat: update toggleSpotLike to use new version tRPC

--- apps/web/src/features/spot/toggleSpotLike/api/toggleLikeSpotAction.ts
+++ apps/web/src/features/spot/toggleSpotLike/api/toggleLikeSpotAction.ts
@@ -1,13 +1,13 @@
 'use server';
 
-import { trpc } from '@/fsd/shared/api/trpc/server';
+import { caller } from '@/fsd/shared/api/trpc/server';
 import { revalidatePath } from 'next/cache';
 
 export async function toggleLikeSpotAction({
 	spotId,
 	userId,
 }: { spotId: string; userId: string }) {
-	const updatedSpot = await trpc.spot.toggleLike({
+	const updatedSpot = await caller.spot.toggleLike({
 		spotId,
 		userId,
 	});
