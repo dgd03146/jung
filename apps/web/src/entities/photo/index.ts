b62b3b2e49361805ea3bee334d229a2290ed16f3
dgd03146 <ibory1220@naver.com>
2025-03-10 11:26:27 +0900

    feat: add TRENDING_PHOTO_PARAMS for trending photo configuration

--- apps/web/src/entities/photo/index.ts
+++ apps/web/src/entities/photo/index.ts
@@ -1,8 +1,5 @@
 export * from './api';
-export {
-	COLLECTION_PARAMS,
-	PHOTO_PARAMS,
-} from './config/constants';
+export * from './config/constants';
 export { transformPhoto } from './lib/transformPhoto';
 export { usePhotoFilterStore } from './model/usePhotoFilterStore';
 export * from './ui';
