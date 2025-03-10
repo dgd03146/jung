6d9656d937b831cd3f8f2dc80fc180a12b8736d0
dgd03146 <ibory1220@naver.com>
2025-03-10 15:19:55 +0900

    refactor: move to category logic to use trpc API

--- apps/server/routes/app.ts
+++ apps/server/routes/app.ts
@@ -1,11 +1,11 @@
 import { createCallerFactory, router } from '../lib/trpc';
+import { categoryRouter } from './category';
 import { guestbookRouter } from './guestbook';
 import { photoCollectionsRouter } from './photo_collections';
 import { photosRouter } from './photos';
 import { postRouter } from './post';
 import { commentRouter } from './post_comment';
 import { spotRouter } from './spot';
-
 export const appRouter = router({
 	post: postRouter,
 	comment: commentRouter,
@@ -13,6 +13,7 @@ export const appRouter = router({
 	photoCollections: photoCollectionsRouter,
 	spot: spotRouter,
 	guestbook: guestbookRouter,
+	category: categoryRouter,
 });
 
 export type AppRouter = typeof appRouter;
