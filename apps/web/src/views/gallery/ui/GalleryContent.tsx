e22a8effd2555876cf15fbe4073de6a71cd19397
dgd03146 <ibory1220@naver.com>
2025-03-19 17:13:20 +0900

    rename: rename gallery page to GalleryContent

---
+++ apps/web/src/views/gallery/ui/GalleryContent.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import {
+	PHOTO_PARAMS,
+	PhotoCard,
+	PhotoList,
+	TRENDING_PHOTO_PARAMS,
+	transformPhoto,
+	usePhotosQuery,
+} from '@/fsd/entities/photo';
+import {
+	EmptyState,
+	LoadingSpinner,
+	useInfiniteScroll,
+	useSearchParamsState,
+} from '@/fsd/shared';
+import { Flex } from '@jung/design-system/components';
+import { useGalleryRouteSync } from '../model/useGalleryRouteSync';
+
+export const GalleryContent = ({ isTrending }: { isTrending?: boolean }) => {
+	useGalleryRouteSync();
+
+	const { q, sort } = useSearchParamsState({
+		defaults: {
+			sort: isTrending ? TRENDING_PHOTO_PARAMS.SORT : PHOTO_PARAMS.SORT,
+			q: PHOTO_PARAMS.QUERY,
+		} as const,
+	});
+
+	const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+		usePhotosQuery({
+			sort,
+			q,
+		});
+
+	const photos = data.pages.flatMap((page) => page.items) ?? [];
+
+	if (photos.length === 0) {
+		return <EmptyState content='photos' />;
+	}
+
+	const formattedPhotos = photos.map((photo) => transformPhoto(photo));
+
+	const { ref } = useInfiniteScroll({
+		fetchNextPage,
+		hasNextPage,
+	});
+
+	return (
+		<>
+			<PhotoList
+				photos={formattedPhotos}
+				renderPhoto={{
+					image: (props, context) => (
+						<PhotoCard imageProps={props} contextProps={context} />
+					),
+				}}
+			/>
+			<Flex justify='center' align='center' minHeight='10' ref={ref}>
+				{isFetchingNextPage && <LoadingSpinner size='small' />}
+			</Flex>
+		</>
+	);
+};
