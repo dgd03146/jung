9501c25bbc2923b8589f48f5d4fdd41335d73885
dgd03146 <ibory1220@naver.com>
2025-03-21 19:50:42 +0900

    refactor: replace lodash with custom pick/omit functions

--- packages/design-system/utils/atoms.ts
+++ packages/design-system/utils/atoms.ts
@@ -1,8 +1,7 @@
 import { elementResets, sprinkles } from '../styles';
 
 import clsx from 'clsx';
-import omit from 'lodash/omit';
-import pick from 'lodash/pick';
+
 import type { Atoms } from '../types/atoms';
 
 /**
@@ -13,11 +12,40 @@ import type { Atoms } from '../types/atoms';
  * The function returns an array with two elements: the Sprinkles props and the remaining props.
  */
 
+function pick<T extends object>(
+	obj: T,
+	keys: string[],
+): Record<string, unknown> {
+	const result: Record<string, unknown> = {};
+
+	for (const key of keys) {
+		if (key in obj) {
+			result[key] = obj[key as keyof T];
+		}
+	}
+
+	return result;
+}
+function omit<T extends object>(obj: T, keys: string[]): Partial<T> {
+	const result = { ...obj };
+
+	for (const key of keys) {
+		if (key in result) {
+			delete result[key as keyof typeof result];
+		}
+	}
+
+	return result;
+}
+
 const keys = Array.from(sprinkles.properties.keys());
-export const extractAtoms = <P extends object>(props: P) => [
-	pick(props, keys),
-	omit(props, keys),
-];
+
+export const extractAtoms = <P extends object>(props: P) => {
+	const atomProps = pick(props, keys);
+	const otherProps = omit(props, keys);
+
+	return [atomProps, otherProps] as const;
+};
 
 /**
  * Generates a class name string based on the provided Atoms object.
