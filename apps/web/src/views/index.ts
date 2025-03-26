c821affb670113ff2fefc391f0691c8fc00d1540
dgd03146 <ibory1220@naver.com>
2025-03-27 02:16:14 +0900

    refactor: separate client component for html streaming in server component

--- apps/web/src/views/index.ts
+++ apps/web/src/views/index.ts
@@ -1,6 +1,6 @@
 export { AboutPage } from './about/ui';
 export * from './blog';
-export { GuestbookPage } from './guestbook/ui';
+export { GuestbookContent } from './guestbook/ui';
 export { HomePage } from './home/ui';
 export { LoginPage } from './login/ui';
 export { SpotDetailPage, SpotsPage } from './spots/ui';
