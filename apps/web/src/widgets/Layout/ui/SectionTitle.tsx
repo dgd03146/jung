680bdafd3cfccb6f6a01350dabbe156764e80590
dgd03146 <ibory1220@naver.com>
2025-05-10 20:00:00 +0900

    refactor(SectionTitle): simplify component structure by removing Flex wrapper

--- apps/web/src/widgets/Layout/ui/SectionTitle.tsx
+++ apps/web/src/widgets/Layout/ui/SectionTitle.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Flex, Typography } from '@jung/design-system/components';
+import { Typography } from '@jung/design-system/components';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { getSection } from '../lib/getSection';
@@ -18,40 +18,7 @@ export function SectionTitle() {
 
 	if (hasSlash) {
 		const [mainSection, subSection] = section.split('/');
-		return (
-			<Flex
-				alignItems='center'
-				gap='2'
-				color='primary'
-				className={styles.sectionTitle}
-			>
-				<Link
-					href={`/${mainSection?.toLowerCase()}`}
-					className={styles.sectionLink}
-				>
-					<Typography.Text level={1} className={styles.sectionText}>
-						{mainSection}
-					</Typography.Text>
-				</Link>
-				<Typography.Text level={1} color='primary'>
-					/
-				</Typography.Text>
-				{subSection === 'PHOTO' ? (
-					<Typography.Text level={1} color='primary'>
-						{subSection}
-					</Typography.Text>
-				) : (
-					<Link
-						href={`/${mainSection?.toLowerCase()}/${subSection?.toLowerCase()}`}
-						className={styles.sectionLink}
-					>
-						<Typography.Text level={1} className={styles.sectionText}>
-							{subSection}
-						</Typography.Text>
-					</Link>
-				)}
-			</Flex>
-		);
+		return null;
 	}
 
 	return (
