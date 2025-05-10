b701497cdf3231a5a86906e4376fdd25ca0cea81
dgd03146 <ibory1220@naver.com>
2025-05-10 19:53:14 +0900

    feat(photo): enhance adjacent and photo query hooks with initial data support
    - Updated `useAdjacentPhotosQuery` to accept `initialData` parameter for improved data handling.
    - Refactored `usePhotoQuery` to include options for `initialData`, `enabled`, `staleTime`, and `gcTime`, allowing for more flexible query configurations.
    - Improved code modularity and clarity by defining types for query parameters.

--- apps/web/src/entities/photo/api/usePhotoQuery.ts
+++ apps/web/src/entities/photo/api/usePhotoQuery.ts
@@ -1,13 +1,25 @@
 import { useTRPC } from '@/fsd/app';
+
+import type { Photo } from '@jung/shared/types';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
-export function usePhotoQuery(id: string) {
+type UsePhotoQueryOptions = {
+	initialData?: Photo;
+	enabled?: boolean;
+
+	staleTime?: number;
+	gcTime?: number;
+};
+
+export function usePhotoQuery(id: string, options?: UsePhotoQueryOptions) {
 	const trpc = useTRPC();
 
-	return useSuspenseQuery(
-		trpc.photos.getPhotoById.queryOptions(id, {
-			staleTime: 1000 * 60 * 60 * 24, // 24시간
-			gcTime: 1000 * 60 * 60 * 48, // 48시간
-		}),
-	);
+	const queryOptions = trpc.photos.getPhotoById.queryOptions(id, {
+		staleTime: 1000 * 60 * 60 * 24,
+		gcTime: 1000 * 60 * 60 * 48,
+
+		...options,
+	});
+
+	return useSuspenseQuery(queryOptions);
 }
