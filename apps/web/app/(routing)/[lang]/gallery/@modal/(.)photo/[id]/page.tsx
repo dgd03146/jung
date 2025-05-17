75744a963c64177a6fc33850b6a34ce495b464fb
dgd03146 <ibory1220@naver.com>
2025-05-18 04:30:48 +0900

    feat(PhotoModal): implement data prefetching and hydration for photo details
    - Added data prefetching for photo details using TRPC to enhance performance and reduce loading times.
    - Wrapped the photo detail page in a HydrationBoundary to ensure proper state management and hydration, improving user experience in modal interactions.

--- apps/web/app/(routing)/[lang]/gallery/@modal/(.)photo/[id]/page.tsx
+++ apps/web/app/(routing)/[lang]/gallery/@modal/(.)photo/[id]/page.tsx
@@ -1,14 +1,24 @@
 import { Modal } from '@/fsd/shared';
+import { getQueryClient, trpc } from '@/fsd/shared/index.server';
 import { PhotoDetailPage, PhotoDetailSkeleton } from '@/fsd/views/gallery';
-
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { Suspense } from 'react';
 
-export default function PhotoModal({ params }: { params: { id: string } }) {
+export default async function PhotoModal({
+	params,
+}: { params: { id: string } }) {
+	const queryClient = getQueryClient();
+	const photoId = params.id;
+
+	queryClient.prefetchQuery(trpc.photos.getPhotoById.queryOptions(photoId));
+
 	return (
 		<Modal>
-			<Suspense fallback={<PhotoDetailSkeleton isModal />}>
-				<PhotoDetailPage photoId={params.id} isModal />
-			</Suspense>
+			<HydrationBoundary state={dehydrate(queryClient)}>
+				<Suspense fallback={<PhotoDetailSkeleton isModal />}>
+					<PhotoDetailPage photoId={photoId} isModal />
+				</Suspense>
+			</HydrationBoundary>
 		</Modal>
 	);
 }
