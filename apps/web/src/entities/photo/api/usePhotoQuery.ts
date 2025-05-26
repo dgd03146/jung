39c7a158f9ed3f5dc3a30c316b11e8ff6f25bdc7
dgd03146 <ibory1220@naver.com>
2025-05-26 14:32:30 +0900

    refactor(usePhotoQuery): simplify interface and remove unnecessary options

--- apps/web/src/entities/photo/api/usePhotoQuery.ts
+++ apps/web/src/entities/photo/api/usePhotoQuery.ts
@@ -1,27 +1,14 @@
 import { useTRPC } from '@/fsd/app';
 
-import type { Photo } from '@jung/shared/types';
-import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
-type UsePhotoQueryOptions = {
-	initialData?: Photo;
-	enabled?: boolean;
-	staleTime?: number;
-	gcTime?: number;
-	suspense?: boolean;
-	select?: <T>(data: Photo) => T;
-};
-
-export function usePhotoQuery(id: string, options?: UsePhotoQueryOptions) {
+export function usePhotoQuery(id: string) {
 	const trpc = useTRPC();
 
-	const queryOptions = trpc.photos.getPhotoById.queryOptions(id, {
-		staleTime: 1000 * 60 * 60 * 24,
-		gcTime: 1000 * 60 * 60 * 48,
-		...options,
-	});
-
-	return options?.suspense === false
-		? useQuery(queryOptions)
-		: useSuspenseQuery(queryOptions);
+	return useSuspenseQuery(
+		trpc.photos.getPhotoById.queryOptions(id, {
+			staleTime: 1000 * 60 * 60 * 24, // 24시간
+			gcTime: 1000 * 60 * 60 * 48, // 48시간
+		}),
+	);
 }
