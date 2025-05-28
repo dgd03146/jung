9c0a449c00f839a6a932a4becfeb1adf972ee8a2
dgd03146 <ibory1220@naver.com>
2025-05-28 14:41:41 +0900

    feat(toggleSpotLike): implement useToggleSpotLike hook for managing spot likes
    - Added useToggleSpotLike hook to handle the like functionality for spots, including optimistic updates for immediate UI feedback.

--- apps/web/src/features/spot/index.ts
+++ apps/web/src/features/spot/index.ts
@@ -1,6 +1,6 @@
 export { FilterSpotCategory } from './filterSpotCategory/ui/FilterSpotCategory';
 export { FilterSpotCategorySkeleton } from './filterSpotCategory/ui/FilterSpotCategorySkeleton';
-export { useToggleSpotLike } from './toggleSpotLike/model/useToggleSpotLike';
+export { useToggleSpotLike } from './toggleSpotLike/api/useToggleSpotLike';
 
 export { ViewMap } from './viewMap/ui/ViewMap';
 
